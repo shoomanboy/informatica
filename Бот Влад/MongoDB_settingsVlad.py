08be464969ce9109b6cb79dbe6efd76f24e2f4ef
shoomanboy <shoomaher2002@mail.ru>
2020-10-28 15:13:27 +0300

    r

--- Бот Влад/MongoDB_settingsVlad.py
+++
@@ -1,4 +0,0 @@
-TG_Token="1328939364:AAHkVONENbuxy5Wf8f0eHGcmmCE2tWuYVXM"
-MONGODB_LINK="mongodb+srv://shoomaher:7598621zhora@telegrambot.fls8z.mongodb.net/telegrambot?retryWrites=true&w=majority"
-CLUSTER="telegrambot"
-MONGODB="translatorbot"
--- Бот Влад/mongodb.py
+++
@@ -1,5 +0,0 @@
-from pymongo import MongoClient
-from MongoDB_settingsVlad import MONGODB, MONGODB_LINK,CLUSTER
-
-
-mdb=MongoClient(MONGODB_LINK)[CLUSTER][MONGODB]
